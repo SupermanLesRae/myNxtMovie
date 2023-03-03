0c637306e878b245fcdbdf2cd7d67eaf77754619
Lesrae <superman.lesrae111@gmail.com>
2023-03-03 19:47:34 +0200

    added front page

---
+++ my-nxt-movie-app/app/loading.jsx
@@ -0,0 +1,4 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center"><p>Loading...</p></div>
+}
